4b280f13257cde16421f5d655934b0082132fbb6
S SABER BASHA <92513033+zero7zero5@users.noreply.github.com>
2023-10-26 19:48:53 +0530

    Edidted Hill Cipher

--- HillCipher.java
+++ HillCipher.java
@@ -1,91 +1,77 @@
+package Sample;
 import java.util.Scanner;
-
 public class HillCipher {
-
     public static void main(String[] args) {
-        Scanner ds = new Scanner(System.in);
-        String k, p;
-        char l;
-        System.out.println("Enter Key : ");
-        k = ds.nextLine();
-        k = k.toLowerCase();
-        while (k.length() < 4) {
-            k += 'a';
-        }
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        int[][] a = new int[2][2];
-        int x = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a 2x2 key matrix (space-separated values, row by row): ");
+        int[][] key = new int[2][2];
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                l = k.charAt(x);
-                a[i][j] = alphabet.indexOf(l);
-                x++;
+                key[i][j] = scanner.nextInt();
             }
         }
-        System.out.println("Enter Plaintext : ");
-        p = ds.nextLine();
+        System.out.print("Enter the plaintext (uppercase letters, no spaces): ");
+        String plaintext = scanner.next();
+        if(plaintext.length()%2!=0) plaintext=plaintext+'a';
+        String ciphertext = encrypt(plaintext, key);
+        System.out.println("Encrypted Text: " + ciphertext);
+        String decryptedText = decrypt(ciphertext, key);
+        System.out.println("Decrypted Text: " + decryptedText);
+        scanner.close();
+    }
 
-        // Encryption
+    public static String encrypt(String plaintext, int[][] key) {
         StringBuilder ciphertext = new StringBuilder();
-        int padding = 0;
-        if (p.length() % 2 != 0) {
-            p += 'x'; 
-            padding = 1;
-        }
-
-        for (int i = 0; i < p.length(); i += 2) {
+        for (int i = 0; i < plaintext.length(); i += 2) {
             int[] block = new int[2];
-            block[0] = alphabet.indexOf(p.charAt(i));
-            block[1] = alphabet.indexOf(p.charAt(i + 1));
-
-            int X = (a[0][0] * block[0] + a[0][1] * block[1]) % 26;
-            int Y = (a[1][0] * block[0] + a[1][1] * block[1]) % 26;
-
-            ciphertext.append(alphabet.charAt(X));
-            ciphertext.append(alphabet.charAt(Y));
+            for (int j = 0; j < 2; j++) {
+                block[j] = plaintext.charAt(i + j) - 'A';
+            }
+            int encryptedChar1 = (key[0][0] * block[0] + key[0][1] * block[1]) % 26;
+            int encryptedChar2 = (key[1][0] * block[0] + key[1][1] * block[1]) % 26;
+            ciphertext.append((char) (encryptedChar1 + 'A'));
+            ciphertext.append((char) (encryptedChar2 + 'A'));
         }
-
-        System.out.println("Ciphertext : " + ciphertext);
-
-        // Decryption
-        String c = ciphertext.toString();
-
+        return ciphertext.toString();
+    }
+    public static String decrypt(String ciphertext, int[][] key) {
         StringBuilder plaintext = new StringBuilder();
-        int[][] aInverse = new int[2][2];
 
-        // Calculate the modular inverse of matrix 'a'
-        int det = (a[0][0] * a[1][1] - a[0][1] * a[1][0] + 26) % 26;
-        int detInverse = 0;
-        for (int i = 0; i < 26; i++) {
+        int det = (key[0][0] * key[1][1] - key[0][1] * key[1][0] + 26) % 26;
+        int detInverse = -1;
+
+        for (int i = 1; i < 26; i++) {
             if ((det * i) % 26 == 1) {
                 detInverse = i;
                 break;
             }
         }
 
-        // Calculate the inverse of matrix 'a'
-        aInverse[0][0] = (a[1][1] * detInverse) % 26;
-        aInverse[0][1] = ((-a[0][1]) * detInverse + 26) % 26;
-        aInverse[1][0] = ((-a[1][0]) * detInverse + 26) % 26;
-        aInverse[1][1] = (a[0][0] * detInverse) % 26;
+        if (detInverse == -1) {
+            System.out.println("Key is not invertible.");
+            return "";
+        }
 
-        for (int i = 0; i < c.length(); i += 2) {
+        int keyInverse[][] = new int[2][2];
+        keyInverse[0][0] = (key[1][1] * detInverse) % 26;
+        keyInverse[0][1] = (26 - key[0][1]) * detInverse % 26;
+        keyInverse[1][0] = (26 - key[1][0]) * detInverse % 26;
+        keyInverse[1][1] = (key[0][0] * detInverse) % 26;
+
+        for (int i = 0; i < ciphertext.length(); i += 2) {
             int[] block = new int[2];
-            block[0] = alphabet.indexOf(c.charAt(i));
-            block[1] = alphabet.indexOf(c.charAt(i + 1));
+            for (int j = 0; j < 2; j++) {
+                block[j] = ciphertext.charAt(i + j) - 'A';
+            }
 
-            int decryptedX = (aInverse[0][0] * block[0] + aInverse[0][1] * block[1]) % 26;
-            int decryptedY = (aInverse[1][0] * block[0] + aInverse[1][1] * block[1]) % 26;
+            int decryptedChar1 = (keyInverse[0][0] * block[0] + keyInverse[0][1] * block[1] + 26) % 26;
+            int decryptedChar2 = (keyInverse[1][0] * block[0] + keyInverse[1][1] * block[1] + 26) % 26;
 
-            plaintext.append(alphabet.charAt((decryptedX + 26) % 26));
-            plaintext.append(alphabet.charAt((decryptedY + 26) % 26));
+            plaintext.append((char) (decryptedChar1 + 'A'));
+            plaintext.append((char) (decryptedChar2 + 'A'));
         }
 
-        // Remove padding characters if necessary
-        if (padding == 1) {
-            plaintext.setLength(plaintext.length() - 1);
-        }
-
-        System.out.println("Decrypted Plaintext : " + plaintext);
+        return plaintext.toString();
     }
+
 }
